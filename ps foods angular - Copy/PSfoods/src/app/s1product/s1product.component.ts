59515fb6665ff65ac46d7699c736ddbbde219132
Reshma <reshmabuduri@gmail.com>
2024-02-01 15:33:43 +0530

    first commit

---
+++ ps foods angular - Copy/PSfoods/src/app/s1product/s1product.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-s1product',
+  templateUrl: './s1product.component.html',
+  styleUrl: './s1product.component.css'
+})
+export class S1productComponent {
+
+}
